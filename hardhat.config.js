7efd30f943d91982b2f4276b2dc806c67e1b9fcb
Murito-01 <ristoo.abrar@gmail.com>
2026-06-08 12:41:43 +0700

    feat: configure Sepolia network and optimize compiler settings using dotenv for environment variables

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,11 @@
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 export default {
@@ -7,8 +14,14 @@ export default {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
+        runs: 999999,
       },
+    },
+  },
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
     },
   },
 };
